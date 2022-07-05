6fce48e7b28c9b92d9408346338e2e99e58185ae
Anzhelika <lientobe@gmail.com>
2022-07-05 06:10:33 +0300

    1st try

--- src/index.js
+++ src/index.js
@@ -23,8 +23,60 @@ export const createStructure = async () => {
   const client = initConnection();
   client.connect();
 
-  // Your code is here...
-  // Your code is here...
+  await client.query(`
+      CREATE TABLE users (
+         id serial PRIMARY KEY,
+         name VARCHAR(30) NOT NULL,
+         date TIMESTAMP NOT NULL DEFAULT now()
+      );
+  `);
+
+  await client.query(`
+      CREATE TABLE categories (
+         id serial PRIMARY KEY,
+         name VARCHAR(30) NOT NULL
+      );
+  `);
+
+  await client.query(`
+      CREATE TABLE authors (
+         id serial PRIMARY KEY,
+         name VARCHAR(30) NOT NULL
+      );
+  `);
+
+  await client.query(`
+      CREATE TABLE books (
+         id serial PRIMARY KEY,
+         title VARCHAR(30) NOT NULL,
+         userid INTEGER NOT NULL,
+         authorid INTEGER NOT NULL,
+         categoryid INTEGER NOT NULL,
+         FOREIGN KEY (userid) REFERENCES users(id) ON DELETE CASCADE,
+         FOREIGN KEY (authorid) REFERENCES authors(id) ON DELETE CASCADE,
+         FOREIGN KEY (categoryid) REFERENCES categories(id) ON DELETE CASCADE
+      );
+  `);
+
+  await client.query(`
+      CREATE TABLE descriptions (
+         id serial PRIMARY KEY,
+         description VARCHAR(10000) NOT NULL,
+         bookid INTEGER UNIQUE NOT NULL,
+         FOREIGN KEY (bookid) REFERENCES books(id) ON DELETE CASCADE
+      );
+  `);
+
+  await client.query(`
+      CREATE TABLE reviews (
+         id serial PRIMARY KEY,
+         message VARCHAR(10000) NOT NULL,
+         userid INTEGER NOT NULL,
+         bookid INTEGER NOT NULL,
+         FOREIGN KEY (userid) REFERENCES users(id) ON DELETE CASCADE,
+         FOREIGN KEY (bookid) REFERENCES books(id) ON DELETE CASCADE
+      );
+  `);
 
   client.end();
 };
@@ -33,7 +85,35 @@ export const createItems = async () => {
   const client = initConnection();
   client.connect();
 
-  // Your code is here...
+  await client.query(`
+      INSERT INTO users (name)
+      VALUES ('Jessie');
+   `);
+
+  await client.query(`
+      INSERT INTO categories (name)
+      VALUES ('Fantasy');
+   `);
+
+  await client.query(`
+      INSERT INTO authors (name)
+      VALUES ('Joe Abercrombie');
+   `);
+
+  await client.query(`
+      INSERT INTO books (title, userid, authorid, categoryid)
+      VALUES ('The Blade Itself', 1, 1, 1);
+   `);
+
+  await client.query(`
+      INSERT INTO descriptions (description, bookid)
+      VALUES ('Murderous conspiracies rise to the surface, old scores are ready to be settled, and the line between hero and villain is sharp enough to draw blood.', 1);
+   `);
+
+  await client.query(`
+      INSERT INTO reviews (message, userid, bookid)
+      VALUES ('The Blade Itself was my first entry into a grimdark fantasy novel that I highly enjoyed; on this reread, I loved it even more.', 1, 1);
+   `);
 
   client.end();
 };
